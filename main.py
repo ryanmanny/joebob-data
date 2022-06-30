54541d79128c8e4771662bebd6420c3a10e8c9d6
Ryan Manny <ryan.w.manny@gmail.com>
2022-06-30 16:53:36 -0700

    Rename var

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 
 SOURCES_PATH = Path('./sources')
-MONSTERVISION_HTML_PATH = SOURCES_PATH / '2018_11_08_cinemassacre_monstervision.html'
+MV_HTML_PATH = SOURCES_PATH / '2018_11_08_cinemassacre_monstervision.html'
 
 
 def compile_monstervision():
-    with open(MONSTERVISION_HTML_PATH) as fp:
+    with open(MV_HTML_PATH) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
 
     raise NotImplementedError()
