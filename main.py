fa306923c46a3883e9751148c6a43cdda43014b2
Ryan Manny <ryan.w.manny@gmail.com>
2022-06-30 16:55:08 -0700

    Add initial logic for Date and Notes

--- main.py
+++ main.py
@@ -3,15 +3,40 @@ from pathlib import Path
 
 from bs4 import BeautifulSoup
 
+from util.dates import parse_date_header
+
 SOURCES_PATH = Path('./sources')
 MV_HTML_PATH = SOURCES_PATH / '2018_11_08_cinemassacre_monstervision.html'
 
 
 def compile_monstervision():
+    episodes = []
+
     with open(MV_HTML_PATH) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
 
-    raise NotImplementedError()
+    start_header = \
+        soup.body.find('h1', text='THE JOE BOB ERA (1996-2000)')
+
+    episode_ps = [
+        p for p in start_header.find_all_next('p')
+        if p.text.strip()
+    ]
+
+    for p in episode_ps:
+        if strongs := p.find_all('strong'):
+            date_header = "".join(s.text for s in strongs).strip()
+            date, note = parse_date_header(date_header)
+        else:
+            print(p.prettify())
+            continue
+
+        episodes.append({
+            'date': date.isoformat(),
+            'note': note,
+        })
+
+    return episodes
 
 
 def compile():
